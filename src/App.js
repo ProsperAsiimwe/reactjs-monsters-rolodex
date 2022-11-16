4b0698d3d56a26deb9a7960e1be33377efe20881
Obi-wan Kadogo <prox0591@gmail.com>
2022-11-16 13:21:31 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
+
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    // initialise the state
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            // console.log(monsters);
+          }
+        )
+      );
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchMonster } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <h1 className="app-title">Monsters Rolodex</h1>
+        <SearchBox
+          className="monsters-search-box"
+          placeholder="Search Monsters"
+          onChangeHandler={onSearchMonster}
+        />
+
+        <CardList myList={filteredMonsters} />
+      </div>
+    );
+  }
+
+  onSearchMonster = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+
+    this.setState(() => {
+      return { searchField };
+    });
+  };
 }
 
 export default App;
